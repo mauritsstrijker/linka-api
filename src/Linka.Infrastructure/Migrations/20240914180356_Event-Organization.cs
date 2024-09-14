c2c28000ac5c224da036c2105d022606365979e5
Maurits <maurits.strijker@outlook.com.br>
2024-09-14 15:04:18 -0300

    migration: event organization

---
+++ src/Linka.Infrastructure/Migrations/20240914180356_Event-Organization.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Linka.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class EventOrganization : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "OrganizationId",
+                table: "Events",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Events_OrganizationId",
+                table: "Events",
+                column: "OrganizationId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Events_Organizations_OrganizationId",
+                table: "Events",
+                column: "OrganizationId",
+                principalTable: "Organizations",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Events_Organizations_OrganizationId",
+                table: "Events");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Events_OrganizationId",
+                table: "Events");
+
+            migrationBuilder.DropColumn(
+                name: "OrganizationId",
+                table: "Events");
+        }
+    }
+}
